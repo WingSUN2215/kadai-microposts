04b4199f4dff39e862dae32514310732c8b14514
wing.sun <wingsun.for.study@gmail.com>
2021-03-21 12:41:43 +0000

    add favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,13 +29,19 @@ class UsersController < ApplicationController
   
   def followings
     @user = User.find(params[:id])
-    @followings = @user.followings.page(params[:id])
+    @followings = @user.followings.page(params[:page])
     counts(@user)
   end
   
   def followers
     @user = User.find(params[:id])
     @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
+  def likes
+     @user = User.find(params[:id])
+    @likes = @user.likes.page(params[:page])
     counts(@user)
   end
   
